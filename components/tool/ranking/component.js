06edfa77fd7e80b358272f5795d4fdcbc778a922
Clément Prod'homme <contact@clementprodhomme.fr>
2020-09-04 17:19:43 +0100

    Fix bug where ranking would be wrongly exported
    This issue would happen in Safari only.

--- components/tool/ranking/component.js
+++ components/tool/ranking/component.js
@@ -6,8 +6,12 @@ import './style.scss';
 const Ranking = ({ rankingData }) => (
   <div className="c-tool-ranking">
     <ul>
-      {rankingData.map(flow => (
+      {rankingData.map((flow, index) => (
         <li key={flow.country}>
+          {/* The position number of the flow can be rendered using a pseudo-element and a CSS
+          counter because html2canvas has issues to render them in Safari:
+          https://github.com/niklasvh/html2canvas/issues/2256 */}
+          <span>{index + 1}</span>
           <span>{flow.country}</span>
           <span>{flow.value}</span>
         </li>
